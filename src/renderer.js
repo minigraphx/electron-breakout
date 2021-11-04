acc634c2b97db074d1fdfaef2ee7f06e30ec3ab9
Andreas Schmidt <support@minigraphx.de>
2021-11-04 15:42:34 +0100

    bounce ball and change colors

--- src/renderer.js
+++ src/renderer.js
@@ -23,23 +23,44 @@ context.closePath();
 
 var ball_x = canvas.width/2;
 var ball_y = canvas.height-30;
+var ballRadius = 10;
+var ballColor = "#0095DD";
+var ballColor2 = "#DD5500";
+var color = "#FFFFFF";
 var dx = 2;
 var dy = -2;
 
 
 function drawBall() {
     context.beginPath();
-    context.arc(ball_x, ball_y, 10, 0, Math.PI*2);
-    context.fillStyle = "#0095DD";
+    context.arc(ball_x, ball_y, ballRadius, 0, Math.PI*2);
+    context.fillStyle = color;
     context.fill();
     context.closePath();
 }
 
+function switchColor() {
+    if(color == ballColor2) {
+        color = ballColor;
+    } else {
+        color = ballColor2;
+    }
+}
+
 function draw() {
     context.clearRect(0, 0, canvas.width, canvas.height);
-    drawBall();
+    if( ball_x-ballRadius <= 0 || ball_x+ballRadius >= canvas.width) {
+        dx = -dx;
+        switchColor();
+    }
+    if(ball_y-ballRadius <= 0 || ball_y+ballRadius >= canvas.height) {
+        dy = -dy;
+        switchColor();
+    }
     ball_x += dx;
     ball_y += dy;
+
+    drawBall();
 }
 
 setInterval(draw, 10);
